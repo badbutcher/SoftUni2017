6af2556375cbc739873862a540b377a6e6c4d763
badbutcher <pavel.p.n@abv.bg>
2017-01-23 15:33:38 +0200

    New! Basic CRUD

---
+++ Databases Basics - MS SQL Server - January 2017/03. Basic CRUD/03. Exercise-Basic-CRUD.sql
@@ -0,0 +1,95 @@
+-- 1. Examine the Databases
+
+--- Part I – Queries for SoftUni Database ---
+
+-- 2. Find All Information About Departments
+
+-- 3. Find all Department Names
+
+-- 4. Find Salary of Each Employee
+
+-- 5. Find Full Name of Each Employee
+
+-- 6. Find Email Address of Each Employee
+
+-- 7. Find All Different Employee’s Salaries
+
+-- 8. Find all Information About Employees
+
+-- 9. Find Names of All Employees by Salary in Range
+
+-- 10. Find Names of All Employees 
+
+-- 11. Find All Employees Without Manager
+
+-- 12. Find All Employees with Salary More Than 50000
+
+-- 13. Find 5 Best Paid Employees.
+
+-- 14. Find All Employees Except Marketing
+
+-- 15. Sort Employees Table
+
+-- 16. Create View Employees with Salaries
+
+-- 17. Create View Employees with Job Titles
+
+-- 18. Distinct Job Titles
+
+-- 19. Find First 10 Started Projects
+
+-- 20. Last 7 Hired Employees
+
+-- 21. Increase Salaries
+
+--- Part II – Queries for Geography Database ---
+
+-- 22. All Mountain Peaks
+
+-- 23. Biggest Countries by Population
+
+-- 24. *Countries and Currency (Euro / Not Euro)
+
+--- Part III – Queries for Diablo Database ---
+
+-- 25. All Diablo Characters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
