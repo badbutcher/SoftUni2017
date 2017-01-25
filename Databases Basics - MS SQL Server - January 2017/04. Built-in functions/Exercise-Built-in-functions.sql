820fcd30322e1042e67fb5ac8149cb0465c8edc5
badbutcher <pavel.p.n@abv.bg>
2017-01-25 17:15:06 +0200

    New exercises

---
+++ Databases Basics - MS SQL Server - January 2017/04. Built-in functions/Exercise-Built-in-functions.sql
@@ -0,0 +1,41 @@
+--- Part I – Queries for SoftUni Database ---
+
+-- Problem 1. Find Names of All Employees by First Name
+
+-- Problem 2. Find Names of All employees by Last Name 
+
+-- Problem 3. Find First Names of All Employees
+
+-- Problem 4. Find All Employees Except Engineers
+
+-- Problem 5. Find Towns with Name Length
+
+-- Problem 6. Find Towns Starting With
+
+-- Problem 7. Find Towns Not Starting With
+
+-- Problem 8. Create View Employees Hired After 2000 Year
+
+-- Problem 9. Length of Last Name
+
+--- Part II – Queries for Geography Database ---
+
+-- Problem 10. Countries Holding ‘A’ 3 or More Times
+
+-- Problem 11. Mix of Peak and River Names
+
+--- Part III – Queries for Diablo Database ---
+
+-- Problem 12. Games from 2011 and 2012 year
+
+-- Problem 13. User Email Providers
+
+-- Problem 14. Get Users with IPAdress Like Pattern
+
+-- Problem 15. Show All Games with Duration and Part of the Day
+
+--- Part IV – Date Functions Queries ---
+
+-- Problem 16. Orders Table
+
+-- Problem 17. People Table
