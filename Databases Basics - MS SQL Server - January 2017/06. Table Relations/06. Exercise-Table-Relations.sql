fa7a0e1e4fe1c56a36770df07dba3fade57bb50c
badbutcher <pavel.p.n@abv.bg>
2017-02-02 11:32:24 +0200

    Table relations DONE

--- Databases Basics - MS SQL Server - January 2017/06. Table Relations/06. Exercise-Table-Relations.sql
+++ Databases Basics - MS SQL Server - January 2017/06. Table Relations/06. Exercise-Table-Relations.sql
@@ -86,37 +86,127 @@ VALUES ('BMW', '07/03/1916'),
 
 -- Problem 3. Many-To-Many Relationship
 CREATE TABLE Students(
-StudentID int,
+StudentID int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
 Name nvarchar(50)
 )
 
 CREATE TABLE Exams(
-ExamID int,
+ExamID int PRIMARY KEY IDENTITY(101, 1) NOT NULL,
 Name varchar(50)
 )
 
-CREATE TABLE [Students Exams](
-StudentID int,
-ExamID int
+CREATE TABLE StudentsExams(
+StudentID int NOT NULL FOREIGN KEY REFERENCES Students(StudentID), 
+ExamID int NOT NULL FOREIGN KEY REFERENCES Exams(ExamID)
+PRIMARY KEY (StudentID, ExamID)
 )
+
+INSERT INTO Students
+VALUES ('Mila'), ('Toni'), ('Ron')
+
+INSERT INTO Exams
+VALUES ('SpringMVC'), ('Neo4j'), ('Oracle 11g')
+
+INSERT INTO StudentsExams
+VALUES(1, 101),(1, 102),(2, 101),(3, 103),(2, 102),(2, 103);
 
 -- Problem 4. Self-Referencing 
 CREATE TABLE Teachers(
-TeacherID int,
+TeacherID int PRIMARY KEY IDENTITY(101,1) NOT NULL,
 Name nvarchar(50),
-ManagerID int
+ManagerID int FOREIGN KEY REFERENCES Teachers(TeacherID)
 )
 
+INSERT INTO Teachers
+VALUES ('John', NULL),
+('Maya', 106),
+('Silvia', 106),
+('Ted', 105),
+('Mark', 101),
+('Greta', 101)
+
 -- Problem 5. Online Store Database
+CREATE TABLE Cities(
+CityID int PRIMARY KEY,
+Name varchar(50)
+)
+
+CREATE TABLE Customers(
+CustomerID int PRIMARY KEY,
+Name varchar(50),
+Birthday date,
+CityID int FOREIGN KEY REFERENCES Cities(CityID)
+)
+
 CREATE TABLE Orders(
-OrderID int,
-CustomerId int
+OrderID int PRIMARY KEY,
+CustomerID int FOREIGN KEY REFERENCES Customers(CustomerID)
+)
+
+CREATE TABLE ItemTypes(
+ItemTypeID int PRIMARY KEY,
+Name varchar(50)
+)
+
+CREATE TABLE Items(
+ItemID int PRIMARY KEY,
+Name varchar(50),
+ItemTypeID int FOREIGN KEY REFERENCES ItemTypes(ItemTypeID)
+)
+
+CREATE TABLE OrderItems(
+OrderID int FOREIGN KEY REFERENCES Orders(OrderId),
+ItemID int FOREIGN KEY REFERENCES Items(ItemID)
+PRIMARY KEY (OrderID, ItemID)
 )
 
 -- Problem 6. University Database
+CREATE TABLE Majors(
+MajorID int PRIMARY KEY,
+Name varchar(50)
+)
+
+CREATE TABLE Subjects(
+SubjectID int PRIMARY KEY,
+SubjectName varchar(50)
+)
+
+CREATE TABLE Students(
+StudentID int PRIMARY KEY,
+StudentNumber int,
+StudentName varchar(50),
+MajorID int FOREIGN KEY REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Agenda(
+StudentID int FOREIGN KEY REFERENCES Students(StudentID),
+SubjectID int FOREIGN KEY REFERENCES Subjects(SubjectID)
+PRIMARY KEY (StudentID, SubjectID)
+)
+
+CREATE TABLE Payments(
+PaymentID int PRIMARY KEY,
+PaymentDate date,
+PaymentAmount decimal,
+StudentID int FOREIGN KEY REFERENCES Students(StudentID)
+)
 
 -- Problem 7. SoftUni Design
+-- DONE
 
 -- Problem 8. Geography Design
+-- DONE
 
 -- Problem 9. *Peaks in Rila
+SELECT MountainRange, PeakName, Elevation FROM
+Mountains JOIN Peaks ON
+Mountains.Id = Peaks.MountainId
+WHERE MountainRange = 'Rila'
+ORDER BY Elevation DESC
+
+-- OR 
+
+SELECT m.MountainRange, p.PeakName, p.Elevation 
+  FROM Mountains AS  m
+  JOIN Peaks As p ON p.MountainId = m.Id
+  WHERE m.MountainRange = 'Rila'  ORDER BY p.Elevation DESC
