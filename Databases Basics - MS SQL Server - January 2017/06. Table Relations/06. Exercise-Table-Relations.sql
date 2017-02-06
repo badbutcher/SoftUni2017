a0b86cc387a8ced2c2e07ec07fa94cc7a7711080
badbutcher <pavel.p.n@abv.bg>
2017-02-06 16:56:53 +0200

    New items

--- Databases Basics - MS SQL Server - January 2017/06. Table Relations/06. Exercise-Table-Relations.sql
+++ Databases Basics - MS SQL Server - January 2017/06. Table Relations/06. Exercise-Table-Relations.sql
@@ -209,4 +209,4 @@ ORDER BY Elevation DESC
 SELECT m.MountainRange, p.PeakName, p.Elevation 
   FROM Mountains AS  m
   JOIN Peaks As p ON p.MountainId = m.Id
-  WHERE m.MountainRange = 'Rila'  ORDER BY p.Elevation DESC
+  WHERE m.MountainRange = 'Rila' ORDER BY p.Elevation DESC
--- Databases Basics - MS SQL Server - January 2017/07. Subqueries and JOINs/07. Exercise-Joins-Subqueries-CTE-and-Indices.sql
+++ Databases Basics - MS SQL Server - January 2017/07. Subqueries and JOINs/07. Exercise-Joins-Subqueries-CTE-and-Indices.sql
@@ -1,14 +1,45 @@
 -- Problem 1. Employee Address
+SELECT TOP(5) e.EmployeeID, e.JobTitle, e.AddressID, a.AddressText 
+FROM Employees AS e
+JOIN Addresses AS a
+ON e.AddressID = a.AddressID
+ORDER BY e.AddressID ASC
 
 -- Problem 2. Addresses with Towns
+SELECT TOP(50) e.FirstName, e.LastName, t.Name, a.AddressText 
+FROM Employees AS e
+JOIN Addresses AS a
+ON e.AddressID = a.AddressID
+JOIN Towns AS t
+ON a.TownID = t.TownID
+ORDER BY e.FirstName ASC, e.LastName ASC 
 
 -- Problem 3. Sales Employee
+SELECT e.EmployeeID, e.FirstName, e.LastName, d.Name
+FROM Employees AS e
+JOIN Departments AS d
+ON e.DepartmentID = d.DepartmentID
+WHERE d.Name = 'Sales'
+ORDER BY e.EmployeeID ASC
 
 -- Problem 4. Employee Departments
+SELECT TOP(5) e.EmployeeID, e.FirstName, e.Salary, d.Name
+FROM Employees AS e
+JOIN Departments AS d
+ON e.DepartmentID = d.DepartmentID
+WHERE e.Salary > 15000
+ORDER BY e.DepartmentID
 
 -- Problem 5. Employees Without Project
+SELECT e.EmployeeID, e.FirstName
+FROM Employees AS e
+
+
 
 -- Problem 6. Employees Hired After
+SELECT e.EmployeeID, e.FirstName, e.LastName, e.HireDate
+FROM Employees AS e
+WHERE e.HireDate > '1.1.1999'
 
 -- Problem 7. Employees with Project
 
