e503a6f67ca7ca816aebc89635e4366be55dcdbe
badbutcher <pavel.p.n@abv.bg>
2017-05-28 11:16:41 +0200

    Commit

---
+++ 03. C# Advanced - May 2017/50. BashSoft/StoryMode/BashSoft/Launcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    class Launcher
+    {
+        static void Main()
+        {
+            StudentsRepository.InitializeData();
+            StudentsRepository.GetStudentScoreFromCourse("Unity", "Ivan");
+        }
+    }
+}
---
+++ 03. C# Advanced - May 2017/50. BashSoft/StoryMode/SimpleJudge/Tester.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BashSoft;
+
+namespace SimpleJudge
+{
+    public static class Tester
+    {
+        public static void CompareContent(string userOutputPath, string expectedOutputPath)
+        {
+            OutputWriter.WriteMessageOnNewLine("Reading files...");
+
+            string mismatchPath = GetMismatchPath(expectedOutputPath);
+        }
+
+        private static string GetMismatchPath(string expectedOutputPath)
+        {
+            int indexOf = expectedOutputPath.LastIndexOf('\\');
+            string directoryPath = expectedOutputPath.Substring(0, indexOf);
+            string finalPath = directoryPath + @"\Mismatches.txt";
+            return finalPath;
+        }
+    }
+}
