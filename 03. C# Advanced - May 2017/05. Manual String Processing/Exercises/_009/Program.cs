be34218639d63cb62022fa5997cb8195a48532e8
badbutcher <pavel.p.n@abv.bg>
2017-06-07 20:33:52 +0200

    05. 99% done

--- 03. C# Advanced - May 2017/05. Manual String Processing/Exercises/_009/Program.cs
+++ 03. C# Advanced - May 2017/05. Manual String Processing/Exercises/_009/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _009
+﻿namespace _009
 {
+    using System;
+    using System.Linq;
+
     class Program
     {
         static void Main()
         {
+            string[] wordsToBan = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string[] text = Console.ReadLine().Split();
+
+            for (int i = 0; i < wordsToBan.Length; i++)
+            {
+                for (int k = 0; k < text.Length; k++)
+                {
+                    if (text[k].Contains(wordsToBan[i]))
+                    {
+                        string word = text[k];
+                        int startIndex = 0;
+                        for (int j = 0; j < word.Length; j++)
+                        {
+                            if (word[j] == wordsToBan[i].First())
+                            {
+                                startIndex = j;
+                                break;
+                            }
+                        }
+
+                        word = word.Substring(startIndex, wordsToBan[i].Length);
+                        string replace = text[k].Replace(word, new string('*', word.Length));
+                        text[k] = replace;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", text));
         }
     }
 }
--- 03. C# Advanced - May 2017/05. Manual String Processing/Exercises/_016/Program.cs
+++ 03. C# Advanced - May 2017/05. Manual String Processing/Exercises/_016/Program.cs
@@ -10,6 +10,96 @@ namespace _016
     {
         static void Main()
         {
+            StringBuilder sb = new StringBuilder();
+            string openTag = "<a";
+            string closeTag = "</a>";
+            string hrefTag = "href";
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+                else
+                {
+                    sb.Append(input);
+                }
+            }
+
+            string text = sb.ToString();
+            int startIndex = text.IndexOf(openTag);
+            while (startIndex != -1)
+            {
+                int endIndex = text.IndexOf(closeTag);
+                if (endIndex == -1)
+                {
+                    break;
+                }
+
+                string aTag = text.Substring(startIndex, endIndex - startIndex + closeTag.Length);
+                string removeTags = aTag.Replace(openTag, string.Empty).Replace(closeTag, string.Empty);
+                text = text.Replace(aTag, removeTags);
+                startIndex = text.IndexOf(openTag);
+
+                int hrefIndex = aTag.IndexOf(hrefTag);
+                string word = string.Empty;
+                
+                for (int i = hrefIndex; i < aTag.Length; i++)
+                {
+                    word += aTag[i];
+                }
+
+                Console.WriteLine(word);
+                word = word.Substring(word.IndexOf(hrefTag)+5);
+                word = word.Trim(new char[] { ' ', '=' });
+                char tagToRemove = word[0];
+                if (tagToRemove == '\"')
+                {
+                    int nextTag = word.IndexOf(tagToRemove, word.IndexOf(tagToRemove) + 1);
+                    word = word.Substring(0, nextTag);
+                    Console.WriteLine(word.TrimStart(tagToRemove));
+                }
+                else if (tagToRemove == '/')
+                {
+                    int nextTag = 0;
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (word[i] == '>')
+                        {
+                            nextTag = word.IndexOf(word[i]);
+                            break;
+                        }
+                    }
+
+                    word = word.Substring(0, nextTag);
+                    Console.WriteLine(word);
+                }
+                else if (tagToRemove == '\'')
+                {
+                    tagToRemove = word[0];
+                    int nextTag = word.IndexOf(tagToRemove, word.IndexOf(tagToRemove) + 1);
+                    word = word.Substring(0, nextTag);
+                    Console.WriteLine(word.TrimStart(tagToRemove));
+                }
+                else
+                {
+                    //int nextTag = 0;
+                    //for (int i = 0; i < word.Length; i++)
+                    //{
+                    //    if (word[i] == ' ')
+                    //    {
+                    //        nextTag = word.IndexOf(word[i]);
+                    //        break;
+                    //    }
+                    //}
+
+
+                    word = word.Substring(word.IndexOf(hrefTag));
+                }
+            }
         }
     }
 }
