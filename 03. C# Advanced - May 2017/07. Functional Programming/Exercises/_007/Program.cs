4a219ebff4fa2a770de4ccd84afab31661a6bb31
badbutcher <pavel.p.n@abv.bg>
2017-06-13 16:02:35 +0200

    07. Functional Programming

---
+++ 03. C# Advanced - May 2017/07. Functional Programming/Exercises/_007/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
