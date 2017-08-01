b84753e24c3b04a0ecf00eba9f4b9946f9f317db
badbutcher <pavel.p.n@abv.bg>
2017-08-01 18:51:55 +0200

    Reflection commit

---
+++ 05. C# OOP Advanced - July 2017/05. Reflection/Lab/_001/_001/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string className, params string[] fields)
+    {
+        Type classType = Type.GetType(className);
+
+        FieldInfo[] publicFields = classType.GetFields(BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+        StringBuilder sb = new StringBuilder();
+
+        object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+        sb.AppendLine($"Class under investigation: {classInstance}");  
+
+        foreach (var item in publicFields.Where(a => fields.Contains(a.Name))) //.GetCustomAttribute<CompilerGeneratedAttribute>() == null)
+        {
+            sb.AppendLine($"{item.Name} = {item.GetValue(classInstance)}");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
