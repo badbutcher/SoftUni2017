d22cd790adb0067efcbc53d87aee7dcbf525f0b3
badbutcher <pavel.p.n@abv.bg>
2018-02-05 20:57:29 +0200

    Commit

--- 09. JS Fundamentals - Jan 2018/98. Exams Preparation/JavaScript Basics Retake - 11 May 2016/Exam/_003 AJAXRequestValidator.js
+++ 09. JS Fundamentals - Jan 2018/98. Exams Preparation/JavaScript Basics Retake - 11 May 2016/Exam/_003 AJAXRequestValidator.js
@@ -1,17 +1,65 @@
 function ajaxRequestValidator(input) {
-    let methodRegex = new RegExp(/^Method:\s+GET|POST|PUT|DELETE$/, 'g');
-    let credentialsRegex = new RegExp(/^Credentials:\s+(Basic |Bearer )[A-Za-z0-9]+$/, 'g');
-    let contentRegex = new RegExp(/^Content: [A-Za-z0-9\.]+$/, 'g')
+    let methodRegex = new RegExp(/^Method: (GET|POST|PUT|DELETE)$/, 'g');
+    let credentialsRegex = new RegExp(/^Credentials: (Basic|Bearer) [A-Za-z0-9]+$/, 'g');
+    let contentRegex = new RegExp(/^Content: [A-Za-z0-9\.]+$/, 'g');
+    let hashPattern = input.pop();
 
+    for (let i = 0; i < input.length - 1; i += 3) {
+        let method = input[i].match(methodRegex)[0].split(' ')[1];
+        let credentials = input[i + 1].match(credentialsRegex)[0].split(' ');
+        let content = input[i + 2].match(contentRegex);
+
+        if (method === null || credentials === null || content === null) {
+            console.log('Response-Code:400');
+        }
+        else if (credentials[1] === 'Basic' && (method === 'POST' || method === 'PUT' || method === 'DELETE')) {
+            console.log(`Response-Method:${method}&Code:401`);
+        }
+        else {
+            let check = true;
+            let re = new RegExp(/([0-9]+)([A-Za-z]+)/, 'g');
+
+            let match;
+            while (match = re.exec(hashPattern)) {
+                let repeating = Number(match[1]);
+                let letter = match[2].toLowerCase();
+                let reCount = new RegExp(letter, 'g');
+                let count = 0;
+                let innerMatch = credentials[2].match(reCount);
+                if (innerMatch !== null) {
+                    count = innerMatch.length;
+                }
+                else {
+                    check = false;
+                }
+
+                if (count === repeating) {
+                    check = true;
+                    break;
+                }
+                else {
+                    check = false;
+                }
+            }
+
+            if (!check) {
+                console.log(`Response-Method:${method}&Code:403`);
+            }
+            else {
+                console.log(`Response-Method:${method}&Code:200&Header:${credentials[2]}`);
+            }
+        }
+    }
 }
 
-ajaxRequestValidator(['Method: GET',
-    'Credentials: Bearer asd918721jsdbhjslkfqwkqiuwjoxXJIdahefJAB',
-    'Content: users.asd.1782452.278asd',
-    'Method: POST',
-    'Credentials: Basic 028591u3jtndkgwndsdkfjwelfqkjwporjqebhas',
-    'Content: Johnathan',
-    '2q'
+ajaxRequestValidator([
+'Method: GET',
+'Credentials: Bearer asd918721jsdbhjslkfwkiuwjoxXJIdahefJAB',
+'Content: users.asd.1782452.278asd',
+'Method: POST',
+'Credentials: Basic 028591u3jtndkgwndsdkfjwelfkjwporjebhas',
+'Content: Johnathan',
+'0q'
 ]);
 
 console.log('\n---\n');
--- 09. JS Fundamentals - Jan 2018/98. Exams Preparation/JavaScript Basics Retake - 11 May 2016/Exam/_004 AngularParser.js
+++ 09. JS Fundamentals - Jan 2018/98. Exams Preparation/JavaScript Basics Retake - 11 May 2016/Exam/_004 AngularParser.js
@@ -1,31 +1,34 @@
 function angularParser(input) {
     let apps = {};
     let appsNotFound = {};
+
     for (let obj of input) {
         let data = obj.split('=').map(a => a.replace(/'/g, ''));
         if (data[0].startsWith('$')) {
             if (data.length > 2) {
                 let type = data[0].replace('$', '');
                 let typeValue = data[1].split('&')[0];
-                let app = data[2];
+                let appName = data[2];
 
-                if (!apps.hasOwnProperty(app)) {
-                    apps[app] = {controllers: [], models: [], views: []};
+                if (apps.hasOwnProperty(appName)) {
+                    addData(type, appName, typeValue, apps);
+                }
+                else if (!appsNotFound.hasOwnProperty(appName)) {
+                    appsNotFound[appName] = {controllers: [], models: [], views: []};
+                    addData(type, appName, typeValue, appsNotFound);
+                }
+                else {
+                    addData(type, appName, typeValue, appsNotFound);
                 }
 
-                if (type === 'controller') {
-                    apps[app].controllers.push(typeValue)
-                }
-                else if (type === 'model') {
-                    apps[app].models.push(typeValue)
-                }
-                else if (type === 'view') {
-                    apps[app].views.push(typeValue)
-                }
             }
             else {
                 let appName = data[1].replace('$', '');
-                if (!apps.hasOwnProperty(appName)) {
+                if (appsNotFound.hasOwnProperty(appName)) {
+                    apps[appName] = {controllers: [], models: [], views: []};
+                    apps[appName] = appsNotFound[appName]
+                }
+                else if (!apps.hasOwnProperty(appName)) {
                     apps[appName] = {controllers: [], models: [], views: []};
                 }
             }
@@ -36,32 +39,52 @@ function angularParser(input) {
         apps[obj].controllers.sort(sortByName);
         apps[obj].models.sort(sortByName);
         apps[obj].views.sort(sortByName);
-        apps[obj].models.sort(function (a, b) {
-            if (apps[a].models === apps[b].models) {
-                return apps[a].models - apps[b].models
-            }
-            else {
-                return apps[b].controllers - apps[a].controllers;
-            }
-        });
     }
 
-    console.log(JSON.stringify(apps));
+    let sort = Object.keys(apps).sort(function (a, b) {
+        if (apps[a].controllers.length === apps[b].controllers.length) {
+            return apps[a].models.length - apps[b].models.length
+        }
+        else {
+            return apps[b].controllers.length - apps[a].controllers.length;
+        }
+    });
+
+
+    let result = {};
+    for (let obj of sort) {
+        result[obj] = apps[obj];
+    }
+
+    console.log(JSON.stringify(result));
 
     function sortByName(a, b) {
         return a.localeCompare(b);
     }
+
+    function addData(type, app, typeValue, appFoundType) {
+        if (type === 'controller') {
+            appFoundType[app].controllers.push(typeValue)
+        }
+        else if (type === 'model') {
+            appFoundType[app].models.push(typeValue)
+        }
+        else if (type === 'view') {
+            appFoundType[app].views.push(typeValue)
+        }
+    }
 }
 
-angularParser(['$app=\'MyApp\'',
-    '$controller=\'My Controller\'&app=\'MyApp\'',
-    '$model=\'My Model\'&app=\'MyApp\'',
-    '$view=\'My View\'&app=\'MyApp\'',
-]);
+// angularParser(['$app=\'MyApp\'',
+//     '$controller=\'My Controller\'&app=\'MyApp\'',
+//     '$model=\'My Model\'&app=\'MyApp\'',
+//     '$view=\'My View\'&app=\'MyApp\'',
+// ]);
 
 console.log('\n---\n');
 
-angularParser(['$controller=\'PHPController\'&app=\'Language Parser\'',
+angularParser([
+    '$controller=\'PHPController\'&app=\'Language Parser\'',
     '$controller=\'JavaController\'&app=\'Language Parser\'',
     '$controller=\'C#Controller\'&app=\'Language Parser\'',
     '$controller=\'C++Controller\'&app=\'Language Parser\'',
