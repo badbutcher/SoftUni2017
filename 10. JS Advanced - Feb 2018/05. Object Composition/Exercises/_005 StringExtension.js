d089dedc3aaaa3a83e96f572b3e299c65127689b
badbutcher <pavel.p.n@abv.bg>
2018-02-26 21:13:50 +0200

    Commit

--- 10. JS Advanced - Feb 2018/05. Object Composition/Exercises/_005 StringExtension.js
+++ 10. JS Advanced - Feb 2018/05. Object Composition/Exercises/_005 StringExtension.js
@@ -1,3 +1,69 @@
-function StringExtension() {
-    
-}
+(function StringExtension() {
+    String.prototype.ensureStart = function (str) {
+        if (!this.startsWith(str)) {
+            return str + this;
+        }
+
+        return this.toString();
+    };
+
+    String.prototype.ensureEnd = function (str) {
+        if (!this.endsWith(str)) {
+            return this + str;
+        }
+
+        return this.toString();
+    };
+
+    String.prototype.isEmpty = function () {
+        return this.length === 0 || this === null;
+    };
+
+    String.prototype.truncate = function (n) {
+        if (this.length <= n) {
+            return this.toString();
+        } else {
+            if (n <= 3) {
+                return '.'.repeat(n);
+            }
+
+            let lastSpace = this.toString().substr(0, n - 2).lastIndexOf(" ");
+
+            if (lastSpace === -1) {
+                lastSpace = n - 3;
+            }
+
+            return this.toString().substr(0, lastSpace) + '...';
+        }
+    };
+
+    String.format = function (input, ...params) {
+        let placeholders = input;
+
+        for (let i = 0; i < params.length; i++) {
+            let re = new RegExp(`\\{${i}\\}`);
+            placeholders = placeholders.replace(re, params[i]);
+        }
+
+        return placeholders;
+    };
+})();
+
+let str = 'the quick brown fox jumps over the lazy dog';
+console.log(str);
+str = str.truncate(10);
+console.log(str);
+str = str.truncate(25);
+console.log(str);
+str = str.truncate(43);
+console.log(str);
+str = str.truncate(45);
+console.log(str);
+console.log();
+console.log();
+let testString = 'the quick brown fox jumps over the lazy dog';
+console.log(testString);
+testString = testString.truncate(6);
+console.log(testString);
+testString = testString.truncate(12);
+console.log(testString);
--- 10. JS Advanced - Feb 2018/05. Object Composition/Exercises/_006 SortedList.js
+++ 10. JS Advanced - Feb 2018/05. Object Composition/Exercises/_006 SortedList.js
@@ -1,3 +1,40 @@
-function sortedList() {
-    
-}
+(function sortedList() {
+    Array.prototype.add = function (element) {
+        sort();
+        this.push(element);
+        return this;
+    };
+
+    Array.prototype.remove = function (index) {
+        sort();
+        this.splice(index, 1);
+        return this;
+    };
+
+    Array.prototype.get = function (index) {
+        sort();
+        console.log(this[index]);
+        return this;
+    };
+
+    Array.prototype.size = function () {
+        sort();
+        console.log(this.length);
+        return this;
+    };
+
+    function sort() {
+        console.log(this);
+        this.sort()
+    }
+})();
+
+let numbers = [5, 4, 3, 2, 1];
+numbers = numbers.add(6);
+console.log(numbers);
+numbers = numbers.remove(5);
+console.log(numbers);
+numbers = numbers.get(3);
+console.log(numbers);
+numbers = numbers.size();
+console.log(numbers);
