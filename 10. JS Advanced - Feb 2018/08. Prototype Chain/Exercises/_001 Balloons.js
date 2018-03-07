1164493a6223cd537e6e1b5d019f03e0dd934eb8
badbutcher <pavel.p.n@abv.bg>
2018-03-07 09:51:22 +0200

    Commit

---
+++ 10. JS Advanced - Feb 2018/08. Prototype Chain/Exercises/_001 Balloons.js
@@ -0,0 +1,42 @@
+function result() {
+    class Balloon {
+        constructor(color, gasWeight) {
+            this.color = color;
+            this.gasWeight = gasWeight;
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength) {
+            super(color, gasWeight);
+            this.ribbonColor = ribbonColor;
+            this.ribbonLength = ribbonLength;
+            this._ribbon = {
+                color: this.ribbonColor,
+                length: this.ribbonLength
+            }
+        }
+
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+            super(color, gasWeight, ribbonColor, ribbonLength);
+            this._text = text;
+        }
+
+        get text() {
+            return this._text;
+        }
+    }
+
+    return {Balloon, PartyBalloon, BirthdayBalloon}
+}
+
+
+let ballon = new Balloon('red', 100);
+let partyBallon = new PartyBalloon('yellow', 50, 'green', 10);
+let burthdayBallon = new BirthdayBalloon('yellow', 50, 'green', 10, 'BD');
---
+++ 10. JS Advanced - Feb 2018/08. Prototype Chain/Lab/_004 ClassHierarchy.js
@@ -0,0 +1,54 @@
+function result() {
+    class Figure {
+        constructor() {
+            if (new.target === Figure) {
+                throw new Error("Cannot instantiate an abstract class.");
+            }
+        }
+
+        toString() {
+            return this.constructor.name;
+        }
+    }
+
+    class Circle extends Figure {
+        constructor(radius) {
+            super();
+            this.radius = radius;
+        }
+
+        get area() {
+            return this.radius * this.radius * Math.PI;
+        }
+
+        toString() {
+            return super.toString() + ` - radius: ${this.radius}`;
+        }
+    }
+
+    class Rectangle extends Figure {
+        constructor(width, height) {
+            super();
+            this.width = width;
+            this.height = height;
+        }
+
+        get area() {
+            return this.width * this.height
+        }
+
+        toString() {
+            return super.toString() + ` - width: ${this.width}, height: ${this.height}`;
+        }
+    }
+
+    return {Figure, Circle, Rectangle}
+}
+
+//let f = new Figure();       //Error
+let c = new Circle(5);
+console.log(c.area);        //78.53981633974483
+console.log(c.toString());  //Circle - radius: 5
+let r = new Rectangle(3, 4);
+console.log(r.area);        //12
+console.log(r.toString());  //Rectangle - width: 3, height: 4
