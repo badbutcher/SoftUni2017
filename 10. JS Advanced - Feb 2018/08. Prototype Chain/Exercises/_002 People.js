8bbb597d6659d200b16cd5641bfed93b73bd0864
badbutcher <pavel.p.n@abv.bg>
2018-03-08 12:03:06 +0200

    Commit

---
+++ 10. JS Advanced - Feb 2018/08. Prototype Chain/Exercises/_002 People.js
@@ -0,0 +1,66 @@
+function result() {
+    class Employee {
+        constructor(name, age) {
+            if (new.target === Employee) {
+                throw new Error("Cannot instantiate an abstract class.");
+            }
+
+            this.name = name;
+            this.age = age;
+            this.salary = 0;
+            this.tasks = [];
+        }
+
+        work() {
+            let currentTask = this.tasks.shift();
+            console.log(`${this.name} ${currentTask}`);
+            this.tasks.push(currentTask);
+        }
+
+        collectSalary() {
+            console.log(`${this.name} received ${this.getSalary()} this month.`);
+        }
+
+        getSalary() {
+            return this.salary;
+        }
+    }
+
+    class Junior extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.tasks.push(`is working on a simple task.`);
+        }
+    }
+
+    class Senior extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.tasks.push(`is working on a complicated task.`);
+            this.tasks.push(`is taking time off work.`);
+            this.tasks.push(`is supervising junior workers.`);
+        }
+    }
+
+    class Manager extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.dividend = 0;
+            this.tasks.push(`scheduled a meeting.`);
+            this.tasks.push(`is preparing a quarterly report.`);
+        }
+
+        getSalary() {
+            return this.salary + this.dividend;
+        }
+    }
+
+    return {Employee, Junior, Senior, Manager}
+}
+
+let res = result();
+
+var guy3 = new res.Manager('bojan', 25);
+guy3.salary = 3000;
+guy3.dividend = 500;
+guy3.collectSalary();
---
+++ 10. JS Advanced - Feb 2018/08. Prototype Chain/Exercises/_003 Posts.js
@@ -0,0 +1,64 @@
+function result() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+
+        toString() {
+            return `Post: ${this.title}\nContent: ${this.content}`
+        }
+    }
+
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = likes;
+            this.dislikes = dislikes;
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+
+        toString() {
+            let result = super.toString() + '\n';
+            let likesDislikes = this.likes - this.dislikes;
+            result += `Rating: ${likesDislikes}`;
+            if (this.comments.length > 0) {
+                result += `\nComments:`;
+                for (let obj of this.comments) {
+                    result += `\n * ${obj}`
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = views;
+        }
+
+        view() {
+            this.views++;
+            return this;
+        }
+
+        toString() {
+            let result = super.toString() + '\n';
+            result += `Views: ${this.views}`;
+            return result;
+        }
+    }
+
+    return {Post, SocialMediaPost, BlogPost}
+}
+
+let classes = result();
+
+let test = new classes.BlogPost("TestTitle", "TestContent", 5);
+test.view().view().view();
