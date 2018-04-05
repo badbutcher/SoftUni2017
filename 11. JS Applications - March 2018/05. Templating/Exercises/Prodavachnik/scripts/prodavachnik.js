eeb311dcbbf61a1ddeb1e85138a057afd3de63f1
badbutcher <pavel.p.n@abv.bg>
2018-04-05 23:40:02 +0200

    Commit

--- 11. JS Applications - March 2018/05. Templating/Exercises/Prodavachnik/scripts/prodavachnik.js
+++ 11. JS Applications - March 2018/05. Templating/Exercises/Prodavachnik/scripts/prodavachnik.js
@@ -93,20 +93,9 @@ function listAds() {
         headers: {'Authorization': 'Kinvey ' + sessionStorage.getItem('authToken')},
     }).then(function (res) {
         showView('viewAds');
-        displayAds(res.reverse())
+        //displayAds(res.reverse());
+        showAllAds(res);
     }).catch(handleAjaxError);
-}
-
-function loadAdForEdit(ad) {
-    showView('viewEditAd');
-    let formId = $('#formEditAd');
-    let form = formId.find('input');
-    form[0].value = ad._id;
-    form[2].value = ad.title;
-    form[3].value = ad.datePublished;
-    form[4].value = ad.price;
-    form[5].value = ad.image;
-    formId.find('textarea')[0].value = ad.description;
 }
 
 function editAd() {
@@ -142,10 +131,11 @@ function editAd() {
     }).catch(handleAjaxError);
 }
 
-function deleteAd(ad) {
+function deleteAd() {
+    let id = $(this).parent().parent().attr('id');
     $.ajax({
         method: 'DELETE',
-        url: BASE_URL + 'appdata/' + APP_KEY + '/ads/' + ad._id,
+        url: BASE_URL + 'appdata/' + APP_KEY + '/ads/' + id,
         headers: {'Authorization': 'Kinvey ' + sessionStorage.getItem('authToken')},
     }).then(function () {
         listAds();
@@ -172,41 +162,41 @@ function viewAd(ad) {
     showView('viewSingleAd');
 }
 
-function displayAds(ads) {
-    let table = $('#ads').find('> table');
-    table.find('tr').each((index, el) => {
-        if (index > 0) {
-            $(el).remove()
-        }
-    });
-
-    for (let i = 0; i < ads.length; i++) {
-        let tr = $('<tr>');
-        table.append($(tr).append($(`<td>${ads[i].title}</td>`))
-            .append($(`<td>${ads[i].publisher}</td>`))
-            .append($(`<td>${ads[i].description}</td>`))
-            .append($(`<td>${ads[i].price}</td>`))
-            .append($(`<td>${ads[i].datePublished}</td>`)));
-
-        if (ads[i]._acl.creator === sessionStorage.getItem('userId')) {
-            $(tr).append(
-                $(`<td>`).append(
-                    $(`<a href="#">[Edit]</a>`).on('click', function () {
-                        loadAdForEdit(ads[i])
-                    })
-                ).append(
-                    $(`<a href="#">[Delete]</a>`).on('click', function () {
-                        deleteAd(ads[i])
-                    })
-                ).append(
-                    $(`<a href="#">[Read More]</a>`).on('click', function () {
-                        viewAd(ads[i])
-                    })
-                )
-            )
-        }
-    }
-}
+// function displayAds(ads) {
+//     let table = $('#ads').find('> table');
+//     table.find('tr').each((index, el) => {
+//         if (index > 0) {
+//             $(el).remove()
+//         }
+//     });
+//
+//     for (let i = 0; i < ads.length; i++) {
+//         let tr = $('<tr>');
+//         table.append($(tr).append($(`<td>${ads[i].title}</td>`))
+//             .append($(`<td>${ads[i].publisher}</td>`))
+//             .append($(`<td>${ads[i].description}</td>`))
+//             .append($(`<td>${ads[i].price}</td>`))
+//             .append($(`<td>${ads[i].datePublished}</td>`)));
+//
+//         if (ads[i]._acl.creator === sessionStorage.getItem('userId')) {
+//             $(tr).append(
+//                 $(`<td>`).append(
+//                     $(`<a href="#">[Edit]</a>`).on('click', function () {
+//                         loadAdForEdit(ads[i])
+//                     })
+//                 ).append(
+//                     $(`<a href="#">[Delete]</a>`).on('click', function () {
+//                         deleteAd(ads[i])
+//                     })
+//                 ).append(
+//                     $(`<a href="#">[Read More]</a>`).on('click', function () {
+//                         viewAd(ads[i])
+//                     })
+//                 )
+//             )
+//         }
+//     }
+// }
 
 function attachAllEvents() {
     // Bind the navigation menu links
@@ -243,8 +233,8 @@ function attachAllEvents() {
 }
 
 function showView(viewName) {
-    $('main > section').hide(); // Hide all views
-    $('#' + viewName).show(); // Show the selected view only
+    $('main > section').hide();
+    $('#' + viewName).show();
 }
 
 function showHideMenuLinks() {
@@ -283,23 +273,95 @@ function showError(errorMsg) {
     errorBox.show();
 }
 
-function showHomeView() {
+async function showHomeView() {
+    let home = await $.get('./templates/viewHome.hbs');
+    let template = Handlebars.compile(home);
+
+    let html = template();
+    $('#viewHome').html(html);
     showView('viewHome');
+    attachAllEvents();
 }
 
-function showLoginView() {
+async function showLoginView() {
     showView('viewLogin');
+    let login = await $.get('./templates/viewLogin.hbs');
+    let template = Handlebars.compile(login);
+
+    let html = template();
+    $('#viewLogin').html(html);
     $('#formLogin').trigger('reset');
+
+    attachAllEvents();
 }
 
-function showRegisterView() {
+async function showRegisterView() {
+    let login = await $.get('./templates/viewRegister.hbs');
+    let template = Handlebars.compile(login);
+
+    let html = template();
+    $('#viewRegister').html(html);
     $('#formRegister').trigger('reset');
     showView('viewRegister');
+    attachAllEvents();
 }
 
-function showCreateAdView() {
+async function showAllAds(ads) {
+    let allAds = await $.get('./templates/viewAds.hbs');
+    let template = Handlebars.compile(allAds);
+    for (let i = 0; i < ads.length; i++) {
+        if (ads[i]._acl.creator === sessionStorage.getItem('userId')) {
+            ads[i].isAuthor = true;
+        }
+    }
+
+    let context = {
+        ads
+    };
+
+    let html = template(context);
+    $('#viewAds').html(html);
+    showView('viewAds');
+
+    $('.deleteAd').on('click', deleteAd);
+    $('.editAd').on('click', showAdForEdit);
+    attachAllEvents();
+}
+
+
+async function showAdForEdit() {
+    showView('viewEditAd');
+    let id = $(this).parent().parent().attr('id');
+    let ad = await $.get('./templates/viewEditAd.hbs');
+    let template = Handlebars.compile(ad);
+    let context = {
+        id: id
+    };
+    console.log(context);
+    let html = template(context);
+    console.log(html);
+    $('#viewEditAd').html(html);
+
+    attachAllEvents();
+    // let formId = $('#formEditAd');
+    // let form = formId.find('input');
+    // form[0].value = ad._id;
+    // form[2].value = ad.title;
+    // form[3].value = ad.datePublished;
+    // form[4].value = ad.price;
+    // form[5].value = ad.image;
+    // formId.find('textarea')[0].value = ad.description;
+}
+
+async function showCreateAdView() {
+    let ad = await $.get('./templates/viewCreateAd.hbs');
+    let template = Handlebars.compile(ad);
+
+    let html = template();
+    $('#viewCreateAd').html(html);
     $('#formCreateAd').trigger('reset');
     showView('viewCreateAd');
+    attachAllEvents();
 }
 
 function signInUser(res, message) {
